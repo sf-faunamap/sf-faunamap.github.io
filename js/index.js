10a17ef2d37d8669ca139da51928381ae46d775f
dnomadb <damon@mapbox.com>
2014-11-15 18:22:03 -0800

    testing data output

--- js/index.js
+++ js/index.js
@@ -17,12 +17,13 @@ var hoverStyle = {
 
 var iconURL = 'img/mapbox-maki-b95bce2/renders/';
 var hash = L.hash(map);
+console.log("You're the man now, dog")
 $.ajax({
     type: "GET",
-    url: "js/data2.geojson"
+    url: "https://gist.githubusercontent.com/dnomadb/40e14a3b78ec118bb711/raw/9b477351ff312a1897cbe631baa093ae7afc92fe/data.geojson"
 }).done(function(data) {
-    console.log(data);
     L.geoJson($.parseJSON(data), {
+        console.log(data);
         style: regStyle,
         onEachFeature: function(feat, layer) {
             layer.bindPopup(
