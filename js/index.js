321f59ea02a5e21cbb4d867e9342dfbb30c923fa
Charles-Antoine Perrault <perrault.ca@gmail.com>
2015-01-23 17:17:51 -0800

    stroke

--- js/index.js
+++ js/index.js
@@ -5,8 +5,8 @@ var map = L.mapbox.map('map', 'caperrault.k82d7bm8', {
 }).fitBounds(bbox);
 
 var regStyle = {
-    stroke: "#F24427",
-    strokeWidth: 2,
+    stroke: true,
+	  weight: 2,
     color: "#F24427",
     fillOpacity: 0.7,
     weight: 0
